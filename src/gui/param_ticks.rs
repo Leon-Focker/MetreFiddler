77ffa2f93d5d7b487a656a24113f7b96a4f0c522
Leon Focker <Leon.Focker@t-online.de>
2026-02-01 21:21:22 +0100

    avoid drawing ticks with duration 0,0

--- src/gui/param_ticks.rs
+++ src/gui/param_ticks.rs
@@ -1,7 +1,8 @@
+use nih_plug::{nih_dbg, nih_log};
 use vizia_plug::vizia::prelude::*;
 use crate::gui::param_binding::ParamBinding;
 use crate::metre::interpolation::interpolation::InterpolationData;
-use crate::util::dry_wet;
+use crate::util::{approx_eq, dry_wet};
 
 #[derive(Lens)]
 pub struct ParamTicks {}
@@ -43,16 +44,22 @@ impl ParamTicks {
                 .height(Pixels(10.0));
 
             for (dur_a, dur_b) in interpolation_data.get(cx).value {
+                let width = dry_wet(dur_a, dur_b, interpolate);
+                let mut draw = width > 0.0;
 
-                Element::new(cx)
-                    .width(Stretch(dry_wet(dur_a, dur_b, interpolate)))
-                    .height(Pixels(10.0));
+                if draw {
+                    Element::new(cx)
+                        .width(Stretch(width))
+                        .height(Pixels(10.0));
+                }
 
                 // TODO would we want the ticks to be different heights depending on indisp_val?
-                Element::new(cx)
-                    .background_color(Color::black())
-                    .width(Pixels(1.0))
-                    .height(Pixels(10.0));
+                if draw {
+                    Element::new(cx)
+                        .background_color(Color::black())
+                        .width(Pixels(1.0))
+                        .height(Pixels(10.0));
+                }
             }
         })
             .padding_left(Pixels(1.0))
